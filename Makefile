1bda05bb07625863135466a412821acbe27b0c68
맹주영 <mjy9088@naver.com>
2023-02-14 00:47:23 +0900

    fix bug

--- Makefile
+++ Makefile
@@ -1,8 +1,5 @@
 test: test.exe
 	./$<
 
-test.exe: exit.o main.o
+test.exe: main.o
 	$(CC) -ldl -o $@ $^
-
-main.o: main.c exit.h
-exit.o: exit.c exit.h
--- main.c
+++ main.c
@@ -6,25 +6,42 @@
 /*   By: Juyeong Maing <jmaing@student.42seoul.kr>  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/13 23:59:38 by Juyeong Maing     #+#    #+#             */
-/*   Updated: 2023/02/14 00:04:00 by Juyeong Maing    ###   ########.fr       */
+/*   Updated: 2023/02/14 00:46:51 by Juyeong Maing    ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "exit.h"
+#include "gnu_source.h"
 
 #include <stdio.h>
 #include <stdbool.h>
 #include <setjmp.h>
+#include <dlfcn.h>
+
+static jmp_buf	*g_jmp_buf = NULL;
+
+void	exit(int status)
+{
+	static void	(*real)(int) = NULL;
+
+	if (!real)
+		real = (void (*)(int))dlsym(RTLD_NEXT, "exit");
+	if (!g_jmp_buf)
+		real(status);
+	else
+		longjmp(*g_jmp_buf, 1);
+}
 
 int	main(void)
 {
-	jmp_buf	buf;
-	int		status;
+	jmp_buf		buf;
+	int			status;
 
-	if (exit_mock(&buf, &status))
+	g_jmp_buf = &buf;
+	status = setjmp(buf);
+	if (status)
 	{
 		printf("Mocked successfully. status: %d\n", status);
-		exit_mock(NULL, NULL);
+		g_jmp_buf = NULL;
 		exit(0);
 	}
 	exit(1);
